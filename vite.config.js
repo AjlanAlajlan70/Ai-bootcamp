55b1d057f1cd3cd89f0a9816bb9a2874da14bfdb
AjlanAjlan70 <444101482@student.ksu.edu.sa>
2026-05-15 09:27:23 +0300

    Add pnpm configuration for only built dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Ai-bootcamp/',
+  base: process.env.GITHUB_ACTIONS ? '/Ai-bootcamp/' : '/',
   plugins: [react()],
 });
